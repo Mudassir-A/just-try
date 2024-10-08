39d15c848c498bf0522d06be069978311f15bf44
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-10-08 16:46:15 +0530

    feat: frontend integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Import react-slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+// Components-Home
+import Home from "./components/Home/Home";
+
+// Authentication Component
+import Login from "./components/Authentication/Login";
+import Signup from "./components/Authentication/Signup";
+
+// Navbar Component
+import Navigation from "./components/Navigation/Navigation";
+
+// Contact Component
+import Contact from "./components/Contact/Contact";
+
+// Men Component
+import Men from "./components/Product/Men";
+// Women Component
+import Women from "./components/Product/Women";
+// BestSeller Component
+import BestSeller from "./components/Product/BestSeller";
+
+// ProductDetails Component
+import ProductDetails from "./components/Product/ProductDetails";
+
+// Recommendation Component
+import Recommandation from "./components/RecommadProd/Recommandation";
+
+// AuthContext
+import AuthContextProvider from "./contexts/AuthContext";
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {/* Wrapping app into AuthContextProvider */}
+      <AuthContextProvider>
+        {/* Routers */}
+        <BrowserRouter>
+          {/* All pages Contain Navbar */}
+          <Navigation />
 
-export default App
+          {/* Path */}
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/men" element={<Men />} />
+            <Route exact path="/women" element={<Women />} />
+            <Route exact path="/bestseller" element={<BestSeller />} />
+            <Route exact path="/men/:title" element={<ProductDetails />} />
+            <Route exact path="/women/:title" element={<ProductDetails />} />
+            <Route path="/:title" element={<ProductDetails />} />
+            <Route exact path="/recommand" element={<Recommandation />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContextProvider>
+    </>
+  );
+};
+
+export default App;
